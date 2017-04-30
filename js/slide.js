3f4d79557b3cfac692effab13d7db630021534cd
johnm60s <orchestration2011@hotmail.com>
2017-04-30 22:03:21 +0900

    second commit

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,4 @@
+
 (function(){
 
 	var prevBtn = document.getElementsByClassName('prevBtn')[0];
@@ -5,58 +6,122 @@
 
 	prevBtn.addEventListener('click', function(){
 
-		console.log('prev Clicked!');
+		SlideShow.prevSlide();
 
 	});
+	nextBtn.addEventListener('click',	function(){
 
-	nextBtn.addEventListener('click', function(){
 
-		console.log('next Clicked!');
+	 	SlideShow.nextSlide();
 
 	});
-
 
 })();
 
 
-var SlideShow =  function(){
 
-		var numSlide = 4,
-				currentSlideIndex = 0,
-				currentSlide,
-				nextSlideIndex,
-				nextSlide,
-				slideImages = document.getElementsByClassName('carousel__img'),
-				bullets = document.getElementsByClassName('bullet'),
-				direction,
-				running = false;
-				slideTarget();
+	var SlideShow = (function(){
 
-		function slideTarget(){
+			var numSlides = 4,
+					currentSlideIndex = 0,
+					running = false,
+					slideImages = document.getElementsByClassName('carousel__img'),
+					bullets = document.getElementsByClassName('bullet'),
+					currentSlide,
+					nextSlide,
+					direction,
+					nextSlideIndex;
 
-			if(direction === 'prev'){
+			function slideMove(newDirection){
 
-				nextSlideIndex = slideImages[currentSlideIndex - 1] === undefined ? numSlide - 1 : currentSlideIndex - 1;
+				if(running){
 
-			}else{
+					return false;
 
-				nextSlideIndex = slideImages[currentSlideIndex + 1] === undefined ?  0 : currentSlideIndex + 1;
+				}
+
+				running = true;
+
+				direction = newDirection;
+				slideTarget(newDirection);
+
+				bullets[currentSlideIndex].classList.add('bullet--active');
+				bullets[nextSlideIndex].classList.remove('bullet--active');
+
+				nextSlide.style.left = direction === 'prev' ? '-100%' : '100%';
+				currentSlide.style.left = '0%';
+
+
+				nextSlide.classList.remove('carousel__img--hidden');
+
+				animate(nextSlide);
+				animate(currentSlide);
+			}
+
+			function slideTarget(){
+
+				if(direction === 'prev'){
+
+					nextSlideIndex = slideImages[currentSlideIndex - 1] === undefined ? (numSlides - 1) : currentSlideIndex - 1;
+
+				}else{
+
+					nextSlideIndex = slideImages[currentSlideIndex + 1] === undefined ?  0 : currentSlideIndex + 1;
+
+				}
+
+				currentSlide = slideImages[currentSlideIndex];
+				nextSlide = slideImages[nextSlideIndex];
+
 
 			}
-			console.log(nextSlideIndex);
-			nextSlide = slideImages[nextSlideIndex];
-			currentSlide = slideImages[currentSlideIndex];
-			console.log(nextSlide);
-			console.log(currentSlide);
-		};
-		function slideAnimation(){
 
-			nextSlide.style.left = direction === 'prev' ? '-100%' : '100%';
-			currentSlide.style.left = '0%';
+			function animate(slideImages){
 
-		}
+				var i = 0;
 
-};
+				var animateInt = setInterval(function(){
 
+						slideImages.style.left = direction === 'prev' ? (parseInt(slideImages.style.left) + 2) + '%': (parseInt(slideImages.style.left) - 2) + '%';
 
-SlideShow();
+						i++;
+						if(i >= 50){
+
+							stopAnimate();
+
+						}
+
+					},7);
+
+				function stopAnimate(){
+
+					currentSlide.classList.add('carousel__img--hidden');
+					nextSlide.style.left = "0%";
+					clearInterval(animateInt);
+					currentSlideIndex = nextSlideIndex;
+					running = false;
+
+				}
+
+			}
+
+			function setContainerSize(){
+
+				var container = document.getElementsByClassName('carousel')[0];
+
+			}
+
+			return{
+
+				nextSlide: function(){
+
+					slideMove('next');
+				},
+
+				prevSlide: function(){
+
+					slideMove('prev');
+
+				}
+			};
+	})();
